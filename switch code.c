7c78b1990bbfbc8fa3c5ffaebdfff1ebb7fa5998
Amit T. Dorle <Amitdorle111@gmail.com>
2024-02-04 00:43:50 +0530

    Switch code added

---
+++ switch code.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+int main()
+{
+    int day=3;
+
+    switch(day)
+    {
+        case 1:
+        {
+            printf("MONDAY");
+            break;
+        }
+        case 2:
+        {
+            printf("Tuesday");
+            break;
+        }
+        case 3:
+        {
+            printf("Wednesday");
+            break;
+        }
+        case 4:
+        {
+            printf("THURSDAY");
+            break;
+        }
+        case 5:
+        {
+            printf("FrIDAY");
+            break;
+        }
+        case 6:
+        {
+            printf("Saturday");
+            break;
+        }
+        case 7:
+        {
+            printf("Sunday");
+            break;
+        }
+    }
+
+    return 0;
+}
