678246d085908f7ca5f4375f6378726882c9d462
Amit T. Dorle <Amitdorle111@gmail.com>
2024-02-05 23:27:53 +0530

    Area of circle is calculated

---
+++ Area of circle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int r;
+    cout<<"Enter radius of circle"<<endl;
+    cin>>r;
+
+    float aoc=3.14*r*r;
+    cout<<"The area of circle for given radius is "<<aoc;
+
+    return 0;
+}
