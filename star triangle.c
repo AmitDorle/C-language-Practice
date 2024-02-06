b0793f50532ff012e37e63095628470fada46bd2
Amit T. Dorle <Amitdorle111@gmail.com>
2024-02-06 15:58:56 +0530

    Star Triangle Pattern

---
+++ star triangle.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter the number of iterations you want:\n");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=n;j>i;j--)
+        {
+            printf(" ");
+        }
+        for(int k=1;k<=i*2-1;k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
