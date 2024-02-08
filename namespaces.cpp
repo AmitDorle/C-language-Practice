2cfcd71334557b34700f4ca841e87e600b4d2caf
Amit T. Dorle <Amitdorle111@gmail.com>
2024-02-08 14:47:39 +0530

    Custom namespace defined

---
+++ namespaces.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+namespace amit
+{
+
+    void show()
+    {
+        cout<<"Show is called";
+    }
+
+}
+
+int main()
+{
+
+    amit::show();
+    return 0;
+}
