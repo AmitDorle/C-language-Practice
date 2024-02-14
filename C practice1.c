78064272957420c806b87e249de470e3733f7b54
Amit T. Dorle <Amitdorle111@gmail.com>
2024-02-15 00:17:48 +0530

    Printing variables of different types

--- C practice1.c
+++ C practice1.c
@@ -9,11 +9,27 @@
 
 
 // PROGRAM 2
+// int main()
+// {
+//     int a=10;
+//     int b=20;
+//     printf("THE addition of a and b is %d",a+b);
+
+//     return 0;
+// }
+
+
+// PROGRAM 3
 int main()
 {
     int a=10;
-    int b=20;
-    printf("THE addition of a and b is %d",a+b);
+    float c=1.4;
+    char ch='a';
+    double b=20;
+    printf("%d\n",a);
+    printf("%f\n",c);
+    printf("%c\n",ch);
+    printf("%lf\n",b);
 
     return 0;
 }
