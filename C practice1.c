ecabc1c43fac649dd5e77ade033a51f0958f3503
Amit T. Dorle <Amitdorle111@gmail.com>
2024-02-15 00:09:08 +0530

    Addition of two numbers

--- C practice1.c
+++ C practice1.c
@@ -1,7 +1,19 @@
 #include<stdio.h>
 
+// PROGRAM 1
+// int main()
+// {
+//     printf("HELLO WORLD");
+//     return 0;
+// }
+
+
+// PROGRAM 2
 int main()
 {
-    printf("HELLO WORLD");
+    int a=10;
+    int b=20;
+    printf("THE addition of a and b is %d",a+b);
+
     return 0;
 }
