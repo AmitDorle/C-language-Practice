215a068631a0d5b31d3c83019c810aa086765536
Amit T. Dorle <Amitdorle111@gmail.com>
2024-02-16 00:43:12 +0530

    While loop practice

--- C practice1.c
+++ C practice1.c
@@ -54,52 +54,66 @@
 
 
 // PROGRAM 5
+// int main()
+// {
+//     int a;
+//     printf("Enter any number from 1 to 7:\n");
+//     scanf("%d",&a);
+//     switch(a)
+//     {
+//         case 1:
+//         {
+//             printf("Sunday");
+//             break;
+//         }
+//         case 2:
+//         {
+//             printf("Monday");
+//             break;
+//         }
+//         case 3:
+//         {
+//             printf("Tuesday");
+//             break;
+//         }
+//         case 4:
+//         {
+//             printf("Wednesday");
+//             break;
+//         }
+//         case 5:
+//         {
+//             printf("Thursday");
+//             break;
+//         }
+//         case 6:
+//         {
+//             printf("Friday");
+//             break;
+//         }
+//         case 7:
+//         {
+//             printf("Saturday");
+//             break;
+//         }
+//         default:
+//         {
+//             printf("Invalid number!!!");
+//         }
+//     }
+//     return 0;
+// }
+
+
+// PROGRAM 6
 int main()
 {
-    int a;
-    printf("Enter any number from 1 to 7:\n");
-    scanf("%d",&a);
-    switch(a)
+    int a=1;
+    while(a<=10)
     {
-        case 1:
-        {
-            printf("Sunday");
-            break;
-        }
-        case 2:
-        {
-            printf("Monday");
-            break;
-        }
-        case 3:
-        {
-            printf("Tuesday");
-            break;
-        }
-        case 4:
-        {
-            printf("Wednesday");
-            break;
-        }
-        case 5:
-        {
-            printf("Thursday");
-            break;
-        }
-        case 6:
-        {
-            printf("Friday");
-            break;
-        }
-        case 7:
-        {
-            printf("Saturday");
-            break;
-        }
-        default:
-        {
-            printf("Invalid number!!!");
-        }
+        printf("%d\n",a);
+        a++;
     }
+
     return 0;
 }
